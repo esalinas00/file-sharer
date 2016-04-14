090000c97db2f65edd3dbc37b9eef8d9c7b91a2e
Che-Wei Lin <linton.tw@gmail.com>
2016-04-14 18:23:58 +0800

    Added POST route for creating a new user

--- app.rb
+++ app.rb
@@ -40,6 +40,20 @@ class FileSharingAPI < Sinatra::Base
     end
   end
 
+  post '/api/v1/users/?' do
+    begin
+      new_data = JSON.parse(request.body.read)
+      saved_user = User.create(new_data)
+    rescue => e
+      logger.info "FAILED to create new user: #{e.inspect}"
+      halt 400
+    end
+
+    new_location = URI.join(@request_url.to_s + '/', saved_user.id.to_s).to_s
+
+    status 201
+    headers('Location' => new_location)
+  end
 
 
   get '/api/v1/files/?' do
