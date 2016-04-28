0bb3751fb7c4720389245359fa8b0fbfb431c001
Robert-Rino <a901002666@gmail.com>
2016-04-29 00:17:21 +0800

    add acounts spec

--- specs/accounts_spec.rb
+++ specs/accounts_spec.rb
@@ -5,7 +5,6 @@ describe 'Testing Project resource routes' do
     Account.dataset.delete
     SimpleFile.dataset.delete
     Folder.dataset.delete
-
   end
 
   describe 'Creating new account' do
@@ -20,8 +19,8 @@ describe 'Testing Project resource routes' do
     it 'SAD: should not create users with duplicate names' do
       req_header = { 'CONTENT_TYPE' => 'application/json' }
       req_body = { username: 'JohnDoe', email: 'demo@gmail.com', password:'1234' }.to_json
-      post '/api/v1/account/', req_body, req_header
-      post '/api/v1/account/', req_body, req_header
+      post '/api/v1/accounts/', req_body, req_header
+      post '/api/v1/accounts/', req_body, req_header
       _(last_response.status).must_equal 400
       _(last_response.location).must_be_nil
     end
@@ -49,22 +48,22 @@ describe 'Testing Project resource routes' do
   end
 
   describe 'Finding existing account' do
-    it 'HAPPY: should find an existing account' do
-      new_user = Account.create(username: 'JohnDoe', email: "JohnDoe@gmail.com", password: "password")
-
-      new_files = (1..3).map do |i|
-        new_user.add_simple_file(f)
-      end
-
-      get "/api/v1/account/JohnDoe"
-      _(last_response.status).must_equal 200
-
-      results = JSON.parse(last_response.body)
-      _(results['data']['attributes']['username']).must_equal new_user.username
-      3.times do |i|
-        _(results['relationships'][i]['id']).must_equal new_files[i].id
-      end
-    end
+    # it 'HAPPY: should find an existing account' do
+    #   new_account = Account.call(username: 'JohnDoe', email: 'JohnDoe@gmail.com', password:'1234')
+    #
+    #   new_floder = (1..3).map do |i|
+    #     new_account.add_owned_folder(name: "Account #{i}")
+    #   end
+    #
+    #   get "/api/v1/account/#{new_account.username}"
+    #   _(last_response.status).must_equal 200
+    #
+    #   results = JSON.parse(last_response.body)
+    #   _(results['data']['username']).must_equal new_account.username
+    #   3.times do |i|
+    #     _(results['relationships'][i]['id']).must_equal new_floder[i].id
+    #   end
+    # end
 
     it 'SAD: should not find non-existent account' do
       get "/api/v1/account/#{invalid_id(Account)}"
@@ -72,12 +71,56 @@ describe 'Testing Project resource routes' do
     end
   end
 
-  # describe 'Getting an index of existing users' do
-  #   it 'HAPPY: should find list of existing users' do
-  #     (1..5).each { |i| Account.create(username: "JaneDoe#{i}", email: "JaneDoe#{i}@gmail.com") }
-  #     result = get '/api/v1/account'
-  #     users = JSON.parse(result.body)
-  #     _(users['data'].count).must_equal 5
-  #   end
-  # end
+  describe 'Creating new forder for account owner' do
+    before do
+      @account = CreateNewAccount.call(
+        username: 'soumya.ray',
+        email: 'sray@nthu.edu.tw',
+        password: 'mypassword')
+    end
+
+    it 'HAPPY: should create a new unique folder for account' do
+      req_header = { 'CONTENT_TYPE' => 'application/json' }
+      req_body = { name: 'Demo Floder', folder_url:'123/456' }.to_json
+      post "/api/v1/accounts/#{@account.username}/folders/",
+           req_body, req_header
+      _(last_response.status).must_equal 201
+      _(last_response.location).must_match(%r{http://})
+    end
+
+    # it 'SAD: should not create folders with duplicate names' do
+    #   req_header = { 'CONTENT_TYPE' => 'application/json' }
+    #   req_body = { name: 'Demo Folder', folder_url:'123/456' }.to_json
+    #   2.times do
+    #     post "/api/v1/accounts/#{@account.username}/folders/",
+    #          req_body, req_header
+    #   end
+    #   _(last_response.status).must_equal 400
+    #   _(last_response.location).must_be_nil
+    # end
+    #
+    # it 'HAPPY: should encrypt relevant data' do
+    #   original_url = 'http://example.org/project/proj.git'
+    #
+    #   fold = @account.add_owned_folder(name: 'Secret Project')
+    #   fold.repo_url = original_url
+    #   fold.save
+    #
+    #   original_desc = 'Secret file with database key'
+    #   original_doc = 'key: 123456789'
+    #   conf = fold.add_simple_file(filename: 'test_file.txt')
+    #   conf.description = original_desc
+    #   conf.document = original_doc
+    #   conf.save
+    #
+    #   _(Folder[fold.id].repo_url).must_equal original_url
+    #   _(Folder[fold.id].repo_url_encrypted).wont_equal original_url
+    #
+    #   _(SimpleFile[conf.id].description).must_equal original_desc
+    #   _(SimpleFile[conf.id].description_encrypted).wont_equal original_desc
+    #
+    #   _(SimpleFile[conf.id].document).must_equal original_doc
+    #   _(SimpleFile[conf.id].document_encrypted).wont_equal original_doc
+    # end
+  end
 end
