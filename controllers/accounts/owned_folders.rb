38ef24c06fe5c0e8beb3632bac6d1d29017e0759
Che-Wei Lin <linton.tw@gmail.com>
2016-06-14 16:56:06 +0800

    create folder api fixed

--- controllers/accounts/owned_folders.rb
+++ controllers/accounts/owned_folders.rb
@@ -1,33 +1,36 @@
 # Sinatra Application Controllers
 class FileSharingAPI < Sinatra::Base
-  post '/api/v1/accounts/:id/owned_folders/?' do
+  post '/api/v1/accounts/:owner_id/folders/?' do
+    content_type 'application/json'
+
     begin
+      # halt 401 unless authorized_account?(env, params[:owner_id])
+
       new_folder_data = JSON.parse(request.body.read)
       saved_folder = CreateFolderForOwner.call(
-        owner_id: params[:id],
+        owner_id: params[:owner_id],
         name: new_folder_data['name'],
         folder_url: new_folder_data['folder_url']
       )
-      new_location = URI.join(@request_url.to_s + '/',
-                              saved_folder.id.to_s).to_s
+
     rescue => e
       logger.info "FAILED to create new folder: #{e.inspect}"
       halt 400
     end
 
     status 201
-    headers('Location' => new_location)
+    saved_folder.to_json
   end
 
-  get '/api/v1/accounts/:owner_id/owned_folders/?' do
-    content_type 'application/json'
-
-    begin
-      owner = Account[params[:owner_id]]
-      JSON.pretty_generate(data: owner.owned_folders)
-    rescue => e
-      logger.info "FAILED to find folders for user #{params[:owner_id]}: #{e}"
-      halt 404
-    end
-  end
+  # get '/api/v1/accounts/:owner_id/owned_folders/?' do
+  #   content_type 'application/json'
+  #
+  #   begin
+  #     owner = Account[params[:owner_id]]
+  #     JSON.pretty_generate(data: owner.owned_folders)
+  #   rescue => e
+  #     logger.info "FAILED to find folders for user #{params[:owner_id]}: #{e}"
+  #     halt 404
+  #   end
+  # end
 end
