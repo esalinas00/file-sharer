b7913b4cae019c31712eda09acbd444e46d03ff3
Erick Salinas <efsalinas00@gmail.com>
2016-06-30 07:09:07 +0800

    Removed puts lines

--- services/import_pk.rb
+++ services/import_pk.rb
@@ -1,11 +1,7 @@
 class ImportPK
   def self.call(account_id:, pk:)
-  	puts 'QUE PUTA PASA'
-  	puts pk
   	account = BaseAccount.where(id: account_id).first
   	account.pk = pk
-  	puts 'Encrypted'
-  	puts account.public_key
     account.save
   end
 end
